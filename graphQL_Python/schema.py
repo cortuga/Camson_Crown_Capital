dbd8b497216a6dd18300e12be6d510c5e0c3eeca
Corey Grandmaison <cgsustainability@gmail.com>
2021-08-09 14:39:25 -0700

    initial set up

--- graphQL_Python/schema.py
+++ graphQL_Python/schema.py
@@ -1,20 +1,57 @@
 import graphene
+import json
+from datetime import datetime
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    last_login = graphene.DateTime(required=False)
 
 class Query(grapheve.ObjectType):
-    is_staff = graphene.Boolean()
+    users = graphene.List(User, first=graphene.Int())
 
-    def resolve_is_staff(self, info):
-        return True
+    def resolve_users(self, info, first):
+        return [
+            User(userName='Alice', last_login=datetime.now()),
+            User(userName='Bob', last_login=datetime.now()),
+            User(userName='steve', last_login=datetime.now()),
+        ][:first]
 
-    schema = graphene.Schema(query=Query)
+class CreateUser(graphene.Mutation):
 
-    restult = schema.execute(
+    class Arguments:
+        username = graphene.string()
+
+    user = graphene.Field(User)
+
+    def mutate(self, info, username)
+        if info.context.get('is_vip'):
+            username = username.upper()
+
+        user = User(username=username)
+        return CreateUser(user=user)
+
+class Mutations(graphene.ObjectType):
+        create_user = CreateUser.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutations)
+
+
+restult = schema.execute(
         '''
-        {
-            isStaff
+        mutation createUser($useranme: String) {
+            createUser(username: "$username") {
+                user {
+                    username
+                }
+            }
         }
-        '''
+        ''',
+        variable_values={'username': 'Alice'},
+        context={'is_vip': False}
     )
 
-items = dictionary(result.data.items())
+
+
+items = dict(result.data.items())
 print(json.dumps(items, indent=4))
